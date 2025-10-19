71840dcbd6ea53207afa0e35fc439153519c25f6
Andrei Grunin <andrei.grunin@majoranatermoli.edu.it>
2025-10-19 21:31:49 +0200

    Add files via upload

---
+++ sommaduenumeri.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    int somma=0;
+    printf("Inserisci i due numeri: ");
+    scanf("%d %d", &a, &b);
+    somma=a+b;
+    printf("La somma e %d", somma);
+    return 0;
+}
